c91d959bf4299945f03167ded3cf57e5915aef85
Xianzhe Liang <liangxianzhe@gmail.com>
2022-08-06 07:25:23 -0700

    Add macos to example.

---
+++ packages/creator/example/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/opt/homebrew/Caskroom/flutter/2.8.0/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/maple/Work/creator/packages/creator/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/maple/Work/creator/packages/creator/example/lib/news_class copy.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/maple/Work/creator/packages/creator/example/.dart_tool/package_config.json"
